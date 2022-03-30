45d8010293f3747123c1d0f01e5e6ddc263fae9a
Tzvi Greenfeld <tzvigreenfield@gmail.com>
2022-03-30 13:46:11 +0300

    untested q1

--- Assignment_1/src/part2/part2.ts
+++ Assignment_1/src/part2/part2.ts
@@ -3,7 +3,17 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countLetters: (s: string) => {} = undefined
+export const countLetters= (s: string) => {
+    R.reduce((chars, currChar) => {
+        if (currChar in chars){
+            chars[currChar]++;
+        }
+        else {
+            chars[currChar] = 1;
+        }
+        return chars;
+    }, Object.create({}), stringToArray(s))
+}
 
 /* Question 2 */
 export const isPaired: (s: string) => boolean = undefined
