f2e828367435a67206994a63073d57e5f32abda8
Tzvi Greenfeld <43534411+TzviGreenfeld@users.noreply.github.com>
2022-04-04 00:50:45 +0300

    Update find.ts

--- Assignment_1/src/part3/find.ts
+++ Assignment_1/src/part3/find.ts
@@ -1,4 +1,6 @@
-import { Result, makeFailure, makeOk, bind, either } from "../lib/result";
+import { ok } from "assert";
+import { findSourceMap } from "module";
+import { Result, makeFailure, makeOk, bind, either, isOk } from "../lib/result";
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
@@ -7,8 +9,16 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     }
     throw "No element found.";
 }
-
-export const findResult = undefined;
+// a
+export const findResult = <T>(pred: (x: T) => boolean, arr: T[] ) : Result<T> =>{
+    if (arr.length === 0){
+        return makeFailure("No such element exists");
+    }else if (pred(arr[0])){
+        return makeOk(arr[0]);
+    }else{
+        findResult(pred, arr.slice(1))
+    }
+}
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -20,6 +30,15 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = undefined;
+// b
+export const returnSquaredIfFoundEven_v2 = (nums: number[]) : Result<number> =>{
+    const res : Result<number> = findResult(x => x%2 === 0, nums);
+    return bind(res, x => makeOk(x*x));
 
-export const returnSquaredIfFoundEven_v3 = undefined;
+}
+
+// c
+export const returnSquaredIfFoundEven_v3 = (nums: number[]) : number =>{
+    const res : Result<number> = findResult(x => x % 2 === 0, nums);
+    return either(res, x => x*x, x => -1);
+}
