afb288137303f31101f3da72f73637d3bdaad277
tzvigr <tzvigreenfield@gmail.com>
2022-04-04 18:37:57 +0300

    finished task2

---
+++ lab2/task3/main.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char encrypt(char c);
+		  
+char decrypt(char c);
+			
+char dprt(char c);
+			  
+char cprt(char c);
+				
+				
+char my_get(char c);
+
+char quit(char c);
+
+char censor(char c)
+{
+	if (c == '!')
+		return '*';
+	else
+		return c;
+}
+
+char *map(char *array, int array_length, char (*f)(char))
+{
+	char *mapped_array = (malloc(array_length * sizeof(char)));
+	int i;
+	for (i = 0; i < array_length - 1; i++)
+	{
+		mapped_array[i] = (f)(array[i]);
+	}
+	mapped_array[i] = 0;
+	return mapped_array;
+}
+
+int main(int argc, char **argv)
+{
+	
+
+	return 0;
+}
+
+char encrypt(char c){
+	if ( ('A' <= c) && (c <= 'z') ) {
+		return c+2;
+	} else{
+		return c;
+	}
+}
+
+char decrypt(char c){
+	if ( ('A' <= c) && (c <= 'z') ) {
+		return c-2;
+	} else{
+		return c;
+	}
+}
+char dprt(char c){
+	printf("%d\n", c);
+	return c;
+}
+
+char cprt(char c){
+	if ( ('A' <= c) && (c <= 'z') ) {
+		printf("%d\n", c);
+	} else{
+		printf("*\n");
+	}
+	return c;
+}
+
+char my_get(char c){
+	return(fgetc(stdin));
+}
+
+char quit(char c){
+	if (c == 'q'){
+		exit(0);
+	}
+	else{
+		return c;
+	}
+}
