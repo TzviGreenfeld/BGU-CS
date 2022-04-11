546adb5634b40842f495476bf339c25daa6a04c1
TzviGreenfeld <tzvigreenfield@gmail.com>
2022-04-12 02:59:33 +0300

    1b not working

--- lab3/Task_1/task1c.c
+++ lab3/Task_1/task1c.c
@@ -9,6 +9,7 @@ typedef struct virus
     char virusName[16];
 } virus;
 
+/*----------1a---------*/
 void readVirus(virus *vir, FILE *input)
 {
     short *size = malloc(sizeof(short));
@@ -32,6 +33,11 @@ void readVirus(virus *vir, FILE *input)
 
 void printVirus(virus *vir, FILE *output)
 {
+    if (vir == 0)
+    {
+        printf("null vir\n");
+        return;
+    }
     int LINE_LEN = 20;
     fprintf(output, "Virus name: %s\n", vir->virusName);
     fprintf(output, "Virus size: %d\n", vir->SigSize);
@@ -53,9 +59,8 @@ void printVirus(virus *vir, FILE *output)
     fprintf(output, "\n");
 }
 
-int main()
+void printAllViruses()
 {
-
     virus *vir = malloc(sizeof(virus));
     FILE *file = fopen("signatures", "rb");
     while (vir)
@@ -68,4 +73,93 @@ int main()
         }
         printVirus(vir, stdout);
     }
+}
+/*---------------------*/
+
+/*----------1b---------*/
+typedef struct link link;
+struct link
+{
+    link *nextVirus;
+    virus *vir;
+};
+
+void list_print(link *virus_list, FILE *output)
+{
+
+    while (virus_list != NULL)
+    {
+        printVirus(virus_list->vir, output);
+        virus_list = virus_list->nextVirus;
+    }
+}
+
+// TODO: test this function!
+link *list_append(link *virus_list, link *to_add)
+{
+    virus_list->nextVirus = to_add;
+    virus_list = to_add;
+    return virus_list;
+    // int addFirst = 1;
+    // if (addFirst)
+    // {
+    //     // add as head
+    //     to_add->nextVirus = virus_list;
+    //     return (to_add);
+    // }
+    // else
+    // {
+    //     // add as tail
+    //     link *lastLink = virus_list;
+    //     do
+    //     {
+    //         lastLink = lastLink->nextVirus;
+    //     } while (lastLink->nextVirus != NULL);
+    //     lastLink->nextVirus = to_add;
+    //     return virus_list;
+    // }
+}
+
+void list_free(link *virus_list)
+{
+    // free list pointers recursively
+    if (virus_list == NULL)
+        return;
+    if (virus_list->vir != NULL)
+    {
+        free(virus_list->vir->sig);
+        free(virus_list->vir);
+    }
+    list_free(virus_list->nextVirus);
+    free(virus_list);
+}
+
+void test1b()
+{
+    FILE *file = fopen("signatures", "rb");
+    link *virList;
+    virList = malloc(sizeof(link));
+    link *head = virList;
+    head->vir = NULL;
+    link *currLink = head;
+    while (!feof(file))
+    {
+        virus *vir = malloc(sizeof(virus));
+        readVirus(vir, file);
+        if (!vir)
+        {
+            link *l = malloc(sizeof(link));
+            l->vir = vir;
+            currLink = list_append(currLink, l);
+        }
+    }
+    virList = head;
+    list_print(virList, stdout);
+    fclose(file);
+}
+/*---------------------*/
+int main()
+{
+    // printAllViruses();
+    test1b();
 }
