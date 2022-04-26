f57a56a1014ae37fce5f2d326c5469082e0c56cd
tzvigr <tzvigreenfield@gmail.com>
2022-04-26 17:35:26 +0300

    q3 testing

--- Assignment_2/ex2/src/q3.ts
+++ Assignment_2/ex2/src/q3.ts
@@ -1,9 +1,12 @@
-import {  Exp, isAtomicExp, isDefineExp, isLetPlusExp, parseL31, Program, LetPlusExp, LetExp, makeLetExp, makeLetPlusExp } from "./L31-ast";
+import { Exp, Program } from "./L31-ast";
 import { Result, makeFailure, makeOk } from "../shared/result";
 import { bind, map, pipe, isEmpty, find } from "ramda";
 //
 import {first, rest} from "../shared/list";
-import { CExp, isAppExp, isCExp, isCompoundExp, isExp, isIfExp, isLetExp, isLitExp, isProcExp, isProgram, makeAppExp, makeDefineExp, makeIfExp, makeProcExp, makeProgram, ProcExp } from "../imp/L3-ast";
+import { LetExp, makeLetExp, makeProgram, CExp, isAppExp, isCExp, isCompoundExp, isExp,
+     isIfExp, isLetExp, isLitExp, isProcExp, isProgram, makeAppExp, makeDefineExp, makeIfExp,
+      makeProcExp, ProcExp,  isAtomicExp, isDefineExp, makeBinding, makeVarDecl, 
+      parseL31,  makeLetPlusExp, LetPlusExp, isLetPlusExp  } from "./L31-ast";
 
 /*
 Purpose: Transform L31 AST to L3 AST
@@ -14,27 +17,29 @@ Type: [Exp | Program] => Result<Exp | Program>
 
 const letPlusToLet = (exp : LetPlusExp ) : LetExp => {
     // const bindings = isEmpty(rest(exp.bindings)) ? first(exp.bindings) : letPlusToLet(makeLetPlusExp(rest(exp.bindings),exp.body));
-    const bindings = [first(exp.bindings)]
+    const key = first(exp.bindings).var.var;
+    const val = first(exp.bindings).val;
+    const bindings = [makeBinding(key, val as CExp)];
     const body = isEmpty(rest(exp.bindings)) ? exp.body : [letPlusToLet(makeLetPlusExp(rest(exp.bindings),exp.body))];
     return makeLetExp(bindings, body);
 }
     
 
-const findLetPlus = (exp: Exp) : Exp => 
+const findLetPlus = (exp: CExp) : CExp => 
     isAtomicExp(exp) ?  exp :
-        isLetExp(exp) ? exp :
+        isLetExp(exp) ? makeLetExp(exp.bindings, map(findLetPlus,exp.body)) :
             isLitExp(exp) ? exp :
-                isDefineExp(exp) ? makeDefineExp( exp.var, findLetPlus(exp.val) as CExp)  : 
-                    isAppExp(exp) ? makeAppExp(findLetPlus(exp.rator) as CExp , map(findLetPlus, exp.rands) as CExp[]) : 
-                        isProcExp(exp) ? makeProcExp(exp.args, map(findLetPlus, exp.body) as CExp[]) :
-                            isIfExp(exp) ? makeIfExp(findLetPlus(exp.test) as CExp, findLetPlus(exp.then) as CExp, findLetPlus(exp.then) as CExp) :
+                    isAppExp(exp) ? makeAppExp(findLetPlus(exp.rator), map(findLetPlus, exp.rands)) : 
+                        isProcExp(exp) ? makeProcExp(exp.args, map(findLetPlus, exp.body)) :
+                            isIfExp(exp) ? makeIfExp(findLetPlus(exp.test), findLetPlus(exp.then) , findLetPlus(exp.then)) :
                                 isLetPlusExp(exp) ? letPlusToLet(exp) :
                                     exp;
 
 
+
 export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> =>{
-    return isExp(exp) ? makeOk(findLetPlus(exp)) :
-                // isProgram(exp) ? makeOk(makeProgram(map(findLetPlus, exp.exps)) :
-                    makeFailure("er"); 
+    return isExp(exp) ? (isDefineExp(exp) ? makeOk(exp): makeOk(findLetPlus(exp))) :
+                isProgram(exp) ? makeOk(makeProgram(map( (exp) => isDefineExp(exp) ? exp: findLetPlus(exp), exp.exps))) :
+                    makeFailure("Error"); 
 } 
 
