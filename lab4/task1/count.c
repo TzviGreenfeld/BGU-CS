cfa02dde51573816ad78a2be876b1a88fec96ad9
tzvigr <tzvigreenfield@gmail.com>
2022-05-04 18:12:25 +0300

    finsihed 1b besides creating file

--- lab4/task1/count.c
+++ lab4/task1/count.c
@@ -3,16 +3,29 @@
 #define STDIN 0
 #define STDOUT 1
 #define STDERR 2
+#define O_WRONLY 01
+#define O_RDWR 02
+#define	O_CREAT	0x0200	
 #define SYS_READ 3
 #define SYS_WRITE 4
+#define OPEN 5
+#define CLOSE 6
+extern int system_call();
 
+void debug(char *er, int DEBUG) {
+    if(DEBUG){
+	    system_call(SYS_WRITE, STDERR, "DEBUG: ", 7);
+	    system_call(SYS_WRITE, STDERR, er, strlen(er));
+	    system_call(SYS_WRITE, STDERR, "\n", 1);
+    }
+}
 
 int main (int argc , char* argv[], char* envp[])
-{
-    extern void system_call();
+{   
     int DEBUG = 0, IN_FILE = 0, OUT_FILE = 0;
     int inStream = STDIN;
 	int outStream = STDOUT;
+
 	
     int i;
 	for(i = 1; i < argc; i++){
@@ -28,19 +41,25 @@ int main (int argc , char* argv[], char* envp[])
 			}
 			fileName[j] = '\0';
 				
-			if (argv[i][1] == 'i'){	
+			if (argv[i][1] == 'i'){
 				IN_FILE = 1;
-				inStream = fopen(fileName, "r");
-
+                debug("input file flag found:", DEBUG);
+                debug(fileName, DEBUG);
+				inStream = system_call(OPEN, fileName, O_RDWR, 0777);
+                
 				if (inStream  == -1){
-                        // ERROR
+                        debug("can't open input file", DEBUG);
 				}
 			}
 			
 			if (argv[i][1] == 'o'){
+                debug("output file flag found:", DEBUG);
+                debug(fileName, DEBUG);
 				OUT_FILE = 1;
-				outStream = fopen(fileName, "w");
+				outStream = system_call(OPEN, fileName, O_WRONLY | O_CREAT);
 			}
+        }
+    }
 
 
     char input[255];
@@ -57,15 +76,22 @@ int main (int argc , char* argv[], char* envp[])
 
     spaceCounter++;
     char wordsCount[32];
-    int j = 0;
+    int t = 0;
     while (spaceCounter > 0)
     {
         int a = spaceCounter % 10;
-        wordsCount[j++] = a | '0';
+        wordsCount[t++] = a | '0';
         spaceCounter /= 10;
     }
 
-    wordsCount[j] = '\n';
+    debug("words counted:", DEBUG);
+    debug(wordsCount, DEBUG);
+    wordsCount[t] = '\n';
     system_call(SYS_WRITE, outStream, wordsCount, 32);
+
+    if(IN_FILE)
+        system_call(CLOSE, inStream);
+    if(OUT_FILE)
+        system_call(CLOSE, outStream);
     return 0;
 }
