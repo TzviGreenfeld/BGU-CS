f29e4e51c994a39cc1115b13a87cf03b20a22b58
tzvigr <tzvigreenfield@gmail.com>
2022-05-23 23:46:26 +0300

    done task 1

--- lab6/task0/b/myshell.c
+++ lab6/task0/b/myshell.c
@@ -12,11 +12,21 @@
 
 #define FASLE 0
 #define TRUE 1
+#define STDIN 0
+#define STDOUT 1
 
 int DEBUG = FASLE;
 
 void cd(cmdLine *lineptr);
 void execute(cmdLine *lineptr);
+
+void debug(char *err)
+{
+    if (DEBUG)
+    {
+        fprintf(stderr, "%s\n", err);
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -27,27 +37,75 @@ int main(int argc, char **argv)
             DEBUG = TRUE;
         }
     }
-    FILE *instream = stdin;
-    char cwd[PATH_MAX];
 
     // get line from user
     char inputBuffer[INPUT_BUFF_SIZE];
     cmdLine *line = parseCmdLines(inputBuffer);
-    while (1)
-    {
-        // get and display current working directory
-        getcwd(cwd, PATH_MAX);
-        printf("%s\n", cwd);
 
-        // get line and parse it
-        fgets(inputBuffer, INPUT_BUFF_SIZE, instream);
-        cmdLine *line = parseCmdLines(inputBuffer);
-        execute(line);
+    int fd[2];
+    if (pipe(fd) == -1)
+    {
+        printf("error in first pipe");
+        exit(1);
+    }
+
+    debug("(parent_process>forking…)");
+    int pid1 = fork();
+    if (DEBUG && pid1 > 0)
+        fprintf(stderr, "(parent_process>created processwith id: %d\n", pid1);
+
+    if (pid1 == -1)
+    {
+        printf("error in first fork");
+        exit(1);
+    }
+    else if (pid1 == 0)
+    { // child1 process
+
+        close(STDOUT);
+        debug("(child1>redirecting stdout to the write end of the pipe…)");
+        dup(fd[1]);
+        close(fd[1]);
+        debug("(child1>going to execute cmd: ls -l)");
+        line = parseCmdLines("ls -l");
+        execvp(line->arguments[0], line->arguments);
         freeCmdLines(line);
+    }
+    else if (pid1 > 0)
+    { // parnet process
+        debug("(parent_process>closing the write end of the pipe…)");
+        close(fd[1]);
+        int pid2 = fork();
+        if (pid2 == -1)
+        {
+            printf("error in first fork");
+            exit(1);
+        }
+        else if (pid2 == 0)
+        { // child2 process
+
+            close(STDIN);
+            dup(fd[0]);
+            debug("(child2>redirecting stdin to the read end of thepipe…)");
+            close(fd[0]);
+            line = parseCmdLines("tail -n 2");
+            debug( "(child2>going to execute cmd: …)");
+            execvp(line->arguments[0], line->arguments);
+            // freeCmdLines(line);
+        }
+        else if (pid2 > 0)
+        { // parent process
+            debug("(parent_process>closing the read end of the pipe…)");
+            close(fd[0]);
+            debug("(parent_process>waiting for child processes to terminate…)");
+            waitpid(pid1, NULL, 0);
+            waitpid(pid2, NULL, 0);
+            debug("(parent_process>exiting…)");
+        }
     }
     return 0;
 }
-
+/*
 void execute(cmdLine *lineptr)
 {
     int instream = stdin;
@@ -82,6 +140,7 @@ void execute(cmdLine *lineptr)
         }
         if (lineptr->outputRedirect != NULL)
         {
+            // invokes the program specified in the cmdline
             close(outstream);
             outstream = open(lineptr->outputRedirect, O_WRONLY | O_CREAT, 0777);
             if (outstream < 0)
@@ -89,7 +148,6 @@ void execute(cmdLine *lineptr)
                 exit(1);
             }
         }
-        // invokes the program specified in the cmdline
         if (execvp(lineptr->arguments[0], lineptr->arguments) == -1)
         {
             perror("executaion failed\n");
@@ -118,3 +176,4 @@ void cd(cmdLine *lineptr)
         perror("CD ERROR\n");
     }
 }
+*/
