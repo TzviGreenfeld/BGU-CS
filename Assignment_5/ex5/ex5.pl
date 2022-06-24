2c52fcc80ee7c8178b13655a0609d28815ec76db
TzviGreenfeld <tzvigreenfield@gmail.com>
2022-06-25 02:07:46 +0300

    prolog done?

--- Assignment_5/ex5/ex5.pl
+++ Assignment_5/ex5/ex5.pl
@@ -13,4 +13,63 @@ maximum_printing_depth(100).
    set_prolog_flag(toplevel_print_options, [max_depth(MPD)|B]).
 
 % Signature: unique(List, UniqueList, Dups)/3
-% Purpose: succeeds if and only if UniqueList contains the same elements of List without duplicates (according to their order in List), and Dups contains the duplicates
+% Purpose: succeeds if and only if UniqueList contains the same elements of List without duplicates
+% (according to their order in List), and Dups contains the duplicates
+
+
+% in if x is in a given list
+in(X, [H|T]) :-
+    X=H; in(X, T).
+
+% this function assume both lists unique
+subset([], [H|T]).
+subset([H|T], L) :-
+    in(H, L),
+    subset(T, L).
+
+% dup test if X is in a list at least twice by finding the first occurence
+% as the head and another in the tail
+dup(X, [H|T]) :-
+    X=H, in(X,T);
+    dup(X, T).
+
+
+% every item in [H|T] appears at least twice in L
+alldups([], L).
+alldups([H|T], L) :-
+    dup(H,L),
+    alldups(T, L).
+    
+
+% if list has unique values
+isunique([]).
+isunique([H|T]) :-
+    not(in(H,T)),
+    isunique(T).
+    
+% S is unique values of L
+setoflist([],[]).
+setoflist(S, L) :-
+    isunique(S),
+   	subset(S, L),
+    subset(L,S).
+    
+    
+
+unique(List, UniqueList, Dups) :-
+    isunique(UniqueList),
+    alldups(Dups, List),
+    subset(List, UniqueList),
+    subset(List, Dups),
+    subset(UniqueList, List),
+    subset(UniqueList, Dups),
+    subset(Dups, List),
+    subset(Dups, UniqueList).
+    
+    
+    
+
+    
+
+
+    
