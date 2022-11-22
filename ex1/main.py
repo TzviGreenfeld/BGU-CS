4339f73e0650b95bd681382a4a64cb1829059a93
tzvig <tzvigreenfield@gmail.com>
2022-11-22 12:10:30 +0200

    some changes

--- ex1/main.py
+++ ex1/main.py
@@ -4,11 +4,32 @@ from nearest_neighbour import learnknn, predictknn, gensmallm
 data = np.load('mnist_all.npz', allow_pickle=True)
 
 
-def get_avg_err(m, k, train_samples, test_samples):
-    # print(f"m={m}")
+labels = [2,3,5,6]
+train_sampels = [data[f"train{i}"] for i in labels]
+test_sampels = np.concatenate([data[f"test{i}"] for i in labels])
+test_lables = np.concatenate([np.full(data[f"test{val}"].shape[0], val) for val in labels]).reshape(-1, 1)
+
+def corrupt_data(X, Y):
+    labels = np.float64(labels)
+    m = X.shape[0]
+    prcnt = 0.15
+    indices = [0,0]
+    while len(indices) != len(np.unique(indices)):
+        indices = np.random.randint(0, high=m, size=int(m * prcnt), dtype=int)
+    
+    for i in indices:
+        curr_label = Y[i]
+        Y[i] = np.random.choice([l for l in labels if l != curr_label])
+    
+    return X,Y
+
+def get_avg_err(m, k, corrupt=False):
+    print(f"m={m}")
     err = []
     for i in range(10):
         (X, Y) = gensmallm(train_sampels, labels, m)
+        if corrupt:
+            (X, Y) = corrupt_data(X, Y)
         classifier = learnknn(k, X, Y)
         predicted = np.array([classifier(sample) for sample in test_sampels]).reshape(-1, 1)
         curr_err = np.count_nonzero(np.array(test_lables != predicted)) / len(predicted)
@@ -23,8 +44,9 @@ def plot_fixed_k_1():
 
     plt.figure(figsize = (10,4))
     ax = plt.axes()
+    title = "MNIST 1-NN error as a function of sample size"
     ax.set(xlabel="sample size", ylabel= "error",
-        title="MNIST 1-NN error as a function of sample size",
+        title=title,
         xticks=sample_size);
 
     bar_width = 1
@@ -34,39 +56,38 @@ def plot_fixed_k_1():
 
     plt.plot(sample_size, avg_errors, linewidth=3)
     plt.legend(["Averege Error over 10 iterations", "Min Error", "Max Error"])
-    plt.show()
+    plt.savefig(f"{title}.png")
 
 
-def plot_fixed_m_200():
+def plot_fixed_m_200(corrupt=False):
     ks = [i for i in range(1,12)]
-    errors = [get_avg_err(200, k) for k in ks]
-    min_err, max_err, avg_err = zip(*errors)
-    
+    err = [get_avg_err(200, k, corrupt=corrupt) for k in ks]
+    min_errors, max_errors, avg_errors = zip(*err)
+
     plt.figure(figsize = (10,4))
     ax = plt.axes()
+    title = "fixed m=200 MNIST error as function of k"
+    if corrupt:
+        title += " (corrupted labels)"
     ax.set(xlabel="k", ylabel= "error",
-        title="fixed m=200 MNIST error as function of k",
+        title=title,
         xticks=ks);
 
     # min/max error bars
     bar_width = 0.15
-    for i, min_err, max_err in zip(ks, min_err, max_err):
+    for i, min_err, max_err in zip(ks, min_errors, max_errors):
         plt.bar(i - bar_width/2, min_err, color='red', alpha=0.5, width=bar_width)
         plt.bar(i + bar_width/2, max_err, color='black',  alpha=0.5, width=bar_width)
 
-    plt.plot(ks, avg_err, linewidth=3)
+    plt.plot(ks, avg_errors, linewidth=3)
     plt.legend(["Averege Error over 10 interations", "Min Error", "Max Error"], loc='best')
-    plt.show()
+    plt.savefig(f"{title}.png")
+
 
 
 
 
 if __name__ == '__main__':
-    k = 1
-    x_train = np.array([[1,2], [3,4], [5,6]])
-    y_train = np.array([1, 0, 1])
-    classifier = learnknn(k, x_train, y_train)
-    x_test = np.array([[10,11], [3.1,4.2], [2.9,4.2], [5,6]])
-    y_testprediction = predictknn(classifier, x_test)
-    print(y_testprediction)
-    # print(y_testprediction == [1, 0, 0, 1])
+    plot_fixed_k_1() # Q 1.a
+    plot_fixed_m_200() # Q 1.e
+    plot_fixed_m_200(corrupt=True) # Q 1.f
