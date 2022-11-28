8d2a916cb497efa061162b550bed11e459adda9e
tzvig <tzvigreenfield@gmail.com>
2022-11-28 17:37:20 +0200

    changed errorBar

--- ex1/main.py
+++ ex1/main.py
@@ -39,7 +39,7 @@ def get_avg_err(m, k, corrupt=False):
 
     return (min(err), max(err), sum(err)/10)
 
-def plot_fixed_k_1(t):
+def plot_fixed_k_1():
     global labels, train_sampels, test_sampels, test_lables
 
     sample_size = np.arange(10, 110, 10)
@@ -53,17 +53,21 @@ def plot_fixed_k_1(t):
         title=title,
         xticks=sample_size);
 
-    bar_width = 1
-    for i, min_err, max_err in zip(sample_size, min_errors, max_errors):
-        plt.bar(i - bar_width/2, min_err, color='red', alpha=0.5, width=bar_width)
-        plt.bar(i + bar_width/2, max_err, color='black',  alpha=0.5, width=bar_width)
+    min_errors = np.array(min_errors)
+    max_errors = np.array(max_errors)
+    avg_errors = np.array(avg_errors)
 
+    plt.errorbar(x=sample_size, y=avg_errors,
+                yerr=np.vstack((avg_errors - min_errors, max_errors - avg_errors)),
+                elinewidth=2,
+                barsabove=True)
+                
     plt.plot(sample_size, avg_errors, linewidth=3)
-    plt.legend(["Averege Error over 10 iterations", "Min Error", "Max Error"])
-    plt.savefig(f"{t}_{title}.png")
+    plt.legend(["Averege Error over 10 iterations", "Min/Max Error"])
+    plt.savefig(f"{title}.png")
 
 
-def plot_fixed_m_200(t,corrupt=False):
+def plot_fixed_m_200(corrupt=False):
     global labels, train_sampels, test_sampels, test_lables
 
     ks = [i for i in range(1,12)]
@@ -81,22 +85,21 @@ def plot_fixed_m_200(t,corrupt=False):
         title=title,
         xticks=ks);
 
-    # min/max error bars
-    bar_width = 0.15
-    for i, min_err, max_err in zip(ks, min_errors, max_errors):
-        plt.bar(i - bar_width/2, min_err, color='red', alpha=0.5, width=bar_width)
-        plt.bar(i + bar_width/2, max_err, color='black',  alpha=0.5, width=bar_width)
+
+    plt.errorbar(x=ks, y=avg_errors,
+            yerr=np.vstack((avg_errors - min_errors, max_errors - avg_errors)),
+            elinewidth=2,
+            barsabove=True)
 
     plt.plot(ks, avg_errors, linewidth=3)
-    plt.legend(["Averege Error over 10 interations", "Min Error", "Max Error"], loc='best')
-    plt.savefig(f"{t}_{title}.png")
+    plt.legend(["Averege Error over 10 interations", "Min/Max Error"], loc='best')
+    plt.savefig(f"{title}.png")
 
 
 
 
 
 if __name__ == '__main__':
-    for i in range(5):
-        plot_fixed_k_1(i) # Q 2.a
-        plot_fixed_m_200(i) # Q 2.e
-        plot_fixed_m_200(i,corrupt=True) # Q 2.f
+    plot_fixed_k_1() # Q 2.a
+    plot_fixed_m_200() # Q 2.e
+    plot_fixed_m_200(corrupt=True) # Q 2.f
