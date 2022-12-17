0b07f34411904cedcfb87b518400061bc59ee7d0
tzvig <tzvigreenfield@gmail.com>
2022-12-17 22:01:05 +0200

    Question 2 plots done

--- ex2/softsvm.py
+++ ex2/softsvm.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 
 # todo: complete the following functions, you may add auxiliary functions or define class to help you
+data = np.load('ex2q2_mnist.npz')
+trainX, testX = data['Xtrain'], data['Xtest']
+trainY, testY = data['Ytrain'], data['Ytest']
+
 
 def softsvm(l, trainX: np.array, trainy: np.array):
     """
@@ -14,24 +18,25 @@ def softsvm(l, trainX: np.array, trainy: np.array):
     """
     m, d = trainX.shape
     u = np.hstack((np.full(d, 0), np.full(m, 1/m)))
-    
-    H = np.zeros((d + m, d + m))
-    # top left of h is 2l*Id_d
-    for i in range(d):
-        H[i][i] = 2 * l
-        
+
+    H = float(2) * l * np.vstack([np.hstack([np.identity(d), np.zeros((d, m))]),
+                                  np.hstack([np.zeros((m, d)), np.zeros((m, m))])])
+    # handle small eigenvalues
+    epsilon = np.finfo(np.float64).eps
+    if min(np.linalg.eigvals(H)) == 0:
+        H = H + (epsilon * np.eye(H.shape[0]))
+
     A_top_left = np.zeros((m, d))
     A_top_right = np.identity(m)
     A_bottom_left = trainX * trainy.reshape(-1, 1)
     A_bottom_right = np.identity(m)
     A = np.vstack([np.hstack([A_top_left, A_top_right]),
                    np.hstack([A_bottom_left, A_bottom_right])])
-    
+
     v = np.hstack((np.zeros(m), np.ones(m)))
     z = solvers.qp(matrix(H), matrix(u), -matrix(A), -matrix(v))
     w = np.array(z["x"])[:d]
     return w
-
 
 
 def simple_test():
@@ -65,6 +70,124 @@ def simple_test():
 
     # this line should print the classification of the i'th test sample (1 or -1).
     print(f"The {i}'th test sample was classified as {predicty}")
+#
+# Q2
+#
+
+
+def predict(w: np.array, testX: np.array):
+    """
+    :param w: linear predictor: numpy array of shape (d, 1)
+    :param testX: samples to calssify: numpy array of shape (m, d)
+    :return: predictions: numpy array of shape (m, 1)
+    """
+    return np.array([[np.sign(example @ w) for example in testX]])
+
+
+def error(real_labels, predicted_labels):
+    """
+    :return: average of difference between two np arrays of shape (1,m)
+    """
+    return np.mean(real_labels != predicted_labels)
+
+
+def get_random_sample(m: int, trainX: np.array, trainY: np.array):
+    """
+    :param m: sample size
+    :param trainX: all samples: numpy array of shape (k >= m, d)
+    :param: trainY: all labels: numpy array of shape (k >=m, 1)
+    :return: _trainX, _trainY: samples and ther labels. numpy arrays of shape (m, d), (m, 1)  
+    """
+    indices = np.random.permutation(trainX.shape[0])
+    _trainX = trainX[indices[:m]]
+    _trainY = trainY[indices[:m]]
+    return (_trainX, _trainY)
+
+
+def get_single_error(m, l):
+    """
+    :param m: sample size
+    :param l: the parameter lambda of the soft SVM algorithm
+    :return: linear predictor w, a numpy array of size (d, 1)
+    """
+    _trainX, _trainY = get_random_sample(m, trainX, trainY)
+    w = softsvm(l, _trainX, _trainY)
+
+    train_error = error(_trainY, predict(w, _trainX).flatten())
+
+    test_error = error(testY, predict(w, testX).flatten())
+
+    return (train_error, test_error)
+
+
+def get_avg_error(m: int, log_lambdas: np.array, times: int):
+    """
+    :param m: sample size
+    :param lambdas: the parameter lambda of the soft SVM algorithm, numpy array
+    :param times: number of times to test for each lambda
+    :return: dictionary of all the calculated values
+    """
+    lambdas = np.power(10, log_lambdas)
+
+    # errors.shape == (l, times)
+    # errors[i][j][0] = train error of the j'th time we ran the expirement with lambdas[i]
+    # errors[i][j][1] = test error of the j'th time we ran the expirement with lambdas[i]
+    errors = np.array([[get_single_error(m, l)
+                      for i in range(times)] for l in lambdas])
+    train_errors = errors[:, :, 0]
+    test_errors = errors[:, :, 1]
+
+    # train_min_values[i] is the minimum train error with lambas[i]
+    train_min_values = np.min(train_errors, axis=1)
+    train_max_values = np.max(train_errors, axis=1)
+    train_avg_values = np.mean(train_errors, axis=1)
+    test_min_values = np.min(test_errors, axis=1)
+    test_max_values = np.max(test_errors, axis=1)
+    test_avg_values = np.mean(test_errors, axis=1)
+
+    return {
+        "log_lambdas": log_lambdas,
+        "train_min_values": train_min_values,
+        "train_max_values": train_max_values,
+        "train_avg_values": train_avg_values,
+        "test_min_values": test_min_values,
+        "test_max_values": test_max_values,
+        "test_avg_values": test_avg_values
+    }
+
+
+def plot(exp1_calc: dict, exp2_calc: dict, title: str):
+
+    plt.figure(figsize=(10, 4))
+    ax = plt.axes()
+    ax.set(xlabel="log λ", ylabel="error",
+           title=title,
+           xticks=exp1_calc["log_lambdas"])
+    # first experiment
+    capsize, alpha = 3, 0.8
+    plt.errorbar(x=exp1_calc["log_lambdas"] + 0.025, y=exp1_calc["train_avg_values"],
+                 yerr=[exp1_calc["train_min_values"],
+                       exp1_calc["train_max_values"]],
+                 label="Train sample average error", capsize=capsize, alpha=alpha)
+
+    plt.errorbar(x=exp1_calc["log_lambdas"] - 0.025, y=exp1_calc["test_avg_values"],
+                 yerr=[exp1_calc["test_min_values"],
+                       exp1_calc["test_max_values"]],
+                 label="Test sample average error", capsize=capsize, alpha=alpha)
+
+    # second experiment
+    plt.scatter(exp2_calc["log_lambdas"],
+                exp2_calc["train_avg_values"], label="Train error")
+    plt.scatter(exp2_calc["log_lambdas"],
+                exp2_calc["test_avg_values"], label="Test error")
+    plt.legend(loc="best")
+    plt.savefig(f"{title}.png")
+
+
+def Q2():
+    experiment1 = get_avg_error(100, np.arange(1, 11), 10)
+    experiment2 = get_avg_error(1000, [1, 3, 5, 8], 1)
+    plot(experiment1, experiment2,  "SVM error as function of λ")
 
 
 if __name__ == '__main__':
