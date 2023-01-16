399cb81ea606a21c1d3433751a5becfc204bae3e
tzvig <tzvigreenfield@gmail.com>
2023-01-17 01:14:45 +0200

    Q1c and Q1d

--- ex3/kmeans.py
+++ ex3/kmeans.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def kmeans(X, k, t):
@@ -10,20 +11,81 @@ def kmeans(X, k, t):
     """
     m, d = X.shape
     C = [np.array([]) for i in range(k)]
-    centroids = np.random.rand(k, d) * 255
+    centroids = X[np.random.choice(range(X.shape[0]), size=k)]
     for _ in range(t):
         for i in range(k):
             distance = [xi - centroids[i] for xi in X]
             min_x = np.argmin([np.linalg.norm(di) for di in distance])
-            for cluster in C:
+            for j,cluster in enumerate(C):
                 if min_x in cluster:
                     cluster = cluster[cluster != min_x]
+                    if len(C[j]) != 0:
+                        centroids[j] = (1.0/len(cluster)) * sum(cluster)
             C[i] = np.append(C[i] ,min_x)
             centroids[i] = (1.0/len(C[i])) * sum(C[i]) 
             
     res = [np.argmin([np.linalg.norm(xi - centroid) for centroid in centroids]) for xi in X ]
     res = np.array(res)
     return res.reshape(-1, 1)
+
+def Q1c():
+    def make_table(data, cols, rows):
+        fig, ax = plt.subplots(figsize=(12, 2))
+        ax.axis('tight')
+        ax.axis('off')
+        the_table = ax.table(cellText=data, colLabels=cols, rowLabels=rows, cellLoc = 'center', loc='center', fontsize=20)
+        # plt.show()
+
+    def analyze_kmeans_unlabeld():
+        X = np.concatenate([data[f'train{i}'] for i in range(10)])
+        Y = np.concatenate(
+            [np.full((data[f'train{i}'].shape[0], 1), i) for i in range(10)])
+
+        indices = np.arange(X.shape[0])
+        np.random.shuffle(indices)
+        samples = np.array(X[indices][:1000])
+        labels = Y[indices][:1000]
+
+        kmeans_res = kmeans(samples, 10, 20)
+
+        clusters = np.empty((10,), dtype=object)
+        for i in range(10):
+            indices = np.where(kmeans_res == i)[0]
+            clusters[i] = indices
+
+        clsuters_size = np.array([len(cluster) for cluster in clusters])
+
+        real_labels_per_cluster = [[labels[i]
+                                    for i in cluster] for cluster in clusters]
+        most_common_label_in_cluster = []
+        for real_labels in real_labels_per_cluster:
+            if len(real_labels) > 0:
+                most_common_label_in_cluster.append(
+                    max(real_labels, key=real_labels.count))
+                print(np.histogram(real_labels, bins=10)[0])
+            else:
+                most_common_label_in_cluster.append(-1)
+        
+        precent_of_most_common_label_in_cluster = [real_labels.count(most_common_label_in_cluster[i]) / clsuters_size[i] for i,real_labels in enumerate(real_labels_per_cluster)]
+
+        errors = []
+        for i in range(10):
+            if clsuters_size[i] == 0:
+                errors.append(0)
+            else:
+                errors.append(np.mean((np.array([most_common_label_in_cluster[i] for _ in range(
+                    clsuters_size[i])]).reshape(-1, 1)) != real_labels_per_cluster[i]))
+
+        row_headers = ["cluster size", "most common label", "%", "error"]
+        col_headers = np.array([f"cluster {i}" for i in range(10)])
+
+        table = np.vstack([clsuters_size, most_common_label_in_cluster, precent_of_most_common_label_in_cluster, errors])
+
+        make_table(table, col_headers, row_headers)
+
+
+    analyze_kmeans_unlabeld()
+
 
 def simple_test():
     # load sample data (this is just an example code, don't forget the other part)
