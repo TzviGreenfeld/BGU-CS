791805b6b5d82611e21ec8a4217494e23cdebfad
TzviGreenfeld <tzvigreenfield@gmail.com>
2023-05-03 16:22:57 +0300

    fixed issues with out of bounds pageNum

--- pages/[pagenum].tsx
+++ pages/[pagenum].tsx
@@ -4,9 +4,12 @@ import Blog from "./index";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const postCount = await prisma.post.count();
-  const pagenum = params?.pagenum;
+  const lastPage = Math.ceil(postCount / 10);
+  // get the page number from the url and bound it to be in range [1, lastPage]
+  let tempPagenum = isNaN(Number(params?.pagenum))? 1 : Math.max(Number(params?.pagenum), 1); // lower bound
+  const pagenum = Math.min(tempPagenum, lastPage); // upper bound
   const feed = await prisma.post.findMany({
-    skip: isNaN(Number(pagenum)) ? 0 : (Number(pagenum) - 1) * 10, // this is the only difference from pages/index.tsx
+    skip: (pagenum - 1) * 10, // this is the only difference from pages/index.tsx
     take: 10,
     where: {
       published: true,
@@ -23,7 +26,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     props: {
       feed: feed,
       postCount: postCount,
-      currpage: pagenum ? Number(pagenum) : 1,
+      currpage: pagenum,
     },
   };
 };
