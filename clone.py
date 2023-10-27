261a92806bc1edd2c89c047139d982694c385b04
Tzvi Greenfeld <tgreenfeld@tenable.com>
2023-10-28 01:37:03 +0300

    adapted the script for subtree

--- clone.py
+++ clone.py
@@ -5,6 +5,8 @@ from concurrent.futures import ThreadPoolExecutor
 
 specific_dir = "front-end-programming-202.1.3091"
 
+repo = "https://github.com/TzviGreenfeld/hw1-blog"
+
 full_path = join(dirname(abspath(__file__)), specific_dir)
 
 if not os.path.exists(full_path):
@@ -13,16 +15,23 @@ if not os.path.exists(full_path):
 os.chdir(full_path)
 
 
-def clone_branch(github_url, branch_name):
+subprocess.run(["git", "remote", "add", "-f", "source_repo", repo], check=True)
+
+
+def add_subtree(branch_name):
+    target_dir = join(full_path, branch_name)
+
     command = [
         "git",
-        "clone",
-        "--single-branch",
-        "--branch",
+        "subtree",
+        "add",
+        "--prefix",
+        target_dir,
+        "source_repo",
         branch_name,
-        github_url,
-        branch_name,
+        "--squash"
     ]
+
     proc = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = proc.communicate()
@@ -30,7 +39,6 @@ def clone_branch(github_url, branch_name):
 
 
 def main():
-    github_url = "https://github.com/TzviGreenfeld/hw1-blog"
     branches_to_clone = [
         "Assignment4-vulnerable",
         "Assignment4-protected",
@@ -40,7 +48,7 @@ def main():
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         for branch_name in branches_to_clone:
-            executor.submit(clone_branch, github_url, branch_name)
+            executor.submit(add_subtree, branch_name)
 
 
 if __name__ == "__main__":
