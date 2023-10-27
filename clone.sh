67f25f4ab333c71572c4a537f8c1dd70c2fe85f8
Tzvi Greenfeld <tgreenfeld@tenable.com>
2023-10-28 02:12:08 +0300

    visual comp

--- clone.sh
+++ clone.sh
@@ -1,3 +1,4 @@
-git remote add -f introduction-to-machine-learning-202.1.3101 https://github.com/TzviGreenfeld/Introduction-To-Machine-Learning
+git remote add -f advenced-computer-graphic-202.1.4901 https://github.com/TzviGreenfeld/Topics-in-Graphics-and-Visual-Computing
 
-git subtree add --prefix=introduction-to-machine-learning-202.1.3101/ introduction-to-machine-learning-202.1.3101 main
+git subtree add --prefix=advenced-computer-graphic-202.1.4901/ advenced-computer-graphic-202.1.4901 main
+
