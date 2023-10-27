36a7b566d03b2d42d52e1a4c4e874077ba4d22d1
Tzvi Greenfeld <tgreenfeld@tenable.com>
2023-10-28 01:23:08 +0300

    modified script

--- clone.py
+++ clone.py
@@ -1,9 +1,16 @@
 import os
 import subprocess
-from os.path import abspath, dirname
+from os.path import abspath, dirname, join
 from concurrent.futures import ThreadPoolExecutor
 
-os.chdir(dirname(abspath(__file__)))
+specific_dir = "front-end-programming-202.1.3091"
+
+full_path = join(dirname(abspath(__file__)), specific_dir)
+
+if not os.path.exists(full_path):
+    os.makedirs(full_path)
+
+os.chdir(full_path)
 
 
 def clone_branch(github_url, branch_name):
@@ -24,8 +31,12 @@ def clone_branch(github_url, branch_name):
 
 def main():
     github_url = "https://github.com/TzviGreenfeld/hw1-blog"
-    branches_to_clone = ["Assignment4-vulnerable",
-                         "Assignment4-protected", "Assignment2", "Assignment3"]
+    branches_to_clone = [
+        "Assignment4-vulnerable",
+        "Assignment4-protected",
+        "Assignment2",
+        "Assignment3"
+    ]
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         for branch_name in branches_to_clone:
